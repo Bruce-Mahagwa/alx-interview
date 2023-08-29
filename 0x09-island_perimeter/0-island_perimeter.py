c0fd48eae74f2574371238e3782ddd432d1e0cd3
Bruce-Mahagwa <jacobbruce880@gmail.com>
2023-08-29 06:59:56 -0700

    Finds perimeter of a grid

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -8,14 +8,16 @@ def island_perimeter(grid):
 
     width = 1
     height = 0
+    maxw = -101
     for i in range(1, len(grid) - 1):
         for j in  range(1, len(grid[i]) - 1):
             if grid[i][j] == 1 and grid[i][j + 1] == 1:
                 width += 1
-        if width > 1:
-            break
+        if width >= 1 and width > maxw:
+            maxw = width;
+    width = 1;
     for i in range(1, len(grid) - 1):
         for j in range(1, len(grid[i]) - 1):
             if grid[i][j] == 1 and grid[i][j + 1] != 1:
                 height += 1
-    return width* 2 + height * 2
+    return maxw * 2 + height * 2
