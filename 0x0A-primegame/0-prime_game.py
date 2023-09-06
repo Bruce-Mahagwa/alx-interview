6de98716514bc3cfe2f5b26c322c4d8bc40bc7c1
Bruce-Mahagwa <jacobbruce880@gmail.com>
2023-09-06 02:01:50 -0700

    Finds the winner in a repetitive game

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+""" finds out if a number is even or not"""
+
+def isEven(n):
+    """ checks if a number is even"""
+    if n % 2 == 0:
+        return True
+    return False
+
+""" checks if a number is prime"""
+
+def isPrime(n):
+    """ returns true if a number is prime"""
+    if n == 1:
+        return False
+    if n == 2 or n == 3:
+        return True
+    prime = True
+    for i in range(2, round(n ** 0.5 + 1)):
+        if n % i == 0:
+            prime = False
+            break
+    return prime
+
+"""Finds the optimal outcome of a game between 2 player"""
+
+
+def isWinner(x, nums):
+    """Finds the winner of a game based on repetition
+        returns winner's name
+    """
+    ben = 0
+    maria = 0
+    i = 0
+    while i < len(nums):
+        options = []
+        ele = nums[i]
+        for j in range(1, ele + 1):
+            if (isPrime(j) == True):
+                options.append(j)
+        if len(options) == 0:
+            ben += 1
+        elif isEven(len(options)):
+            ben += 1
+        else: 
+            maria += 1
+        i+=1;
+    if ben > maria:
+        return "Ben"
+    return "Maria"
